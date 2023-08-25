c5df300788c40cd623f23131beb763921b50a3dc
leiah2 <leiah.nay@knowledgehook.com>
2023-08-25 12:02:11 -0400

    same

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 })
 export class ResumeComponent implements AfterViewInit {
 
-  constructor() { }
+  constructor() { 
+
+    if(navigator.userAgent.indexOf('Safari') != - 1 && navigator.userAgent.indexOf('Chrome') == -1) {
+      window.open("/assets/LeiahNayResume.pdf")
+    }
+
+  }
 
   ngAfterViewInit(): void {
   }
