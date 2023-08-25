c3644289faff8d7b4ed5e4e4a88b1624d9718b86
leiah2 <leiah.nay@knowledgehook.com>
2023-08-25 12:22:22 -0400

    try again

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -5,15 +5,15 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css']
 })
-export class ResumeComponent implements AfterViewInit {
+export class ResumeComponent implements OnInit {
 
   constructor() { 
     
 
   }
 
-  ngAfterViewInit(): void {
-    
+  ngOnInit(): void {
+
     if(navigator.userAgent.indexOf('Safari') != - 1 && navigator.userAgent.indexOf('Chrome') == -1) {
       //window.open("/assets/LeiahNayResume.pdf")
       window.location.href = "assets/LeiahNayResume.pdf";
