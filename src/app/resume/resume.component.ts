2e9a6b6caf9e22abe57d91d7a7b00249e62c3e3f
leiah2 <leiah.nay@knowledgehook.com>
2023-12-23 01:52:16 -0500

    resume pop up

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -18,16 +18,19 @@ export class ResumeComponent implements OnInit {
     var notSafari = document.getElementById("not-safari");
     var safari = document.getElementById("safari");
 
-    if(navigator.userAgent.indexOf('Safari') != - 1 && navigator.userAgent.indexOf('Chrome') == -1) {
-      //window.open("/assets/LeiahNayResume.pdf")
-      //window.location.href = "assets/LeiahNayResume.pdf";
 
-      gsap.set(notSafari, {display : "none"});
-    } else {
+    gsap.set(safari, {display : "none"});
+    
+    // if(navigator.userAgent.indexOf('Safari') != - 1 && navigator.userAgent.indexOf('Chrome') == -1) {
+    //   //window.open("/assets/LeiahNayResume.pdf")
+    //   //window.location.href = "assets/LeiahNayResume.pdf";
+
+    //   gsap.set(notSafari, {display : "none"});
+    // } else {
       
-      gsap.set(safari, {display : "none"});
+    //   gsap.set(safari, {display : "none"});
 
-    }
+    // }
 
 
   }
